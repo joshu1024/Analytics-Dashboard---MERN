8cf2889caa397b0c6ff8d09422575649380d240e
josh1024 <joshuakipamet@gmail.com>
2026-02-06 17:46:53 +0300

    Updating cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://dashboard-mern-tau.vercel.app", "http://localhost:5173"],
+    origin: "https://dashboard-mern-tau.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
